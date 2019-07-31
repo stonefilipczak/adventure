f6cf2c5389891cf1125bd5db0cb241af95442b10
Stone Filipczak <stonejacobs@gmail.com>
2019-07-31 13:51:30 -0400

    update DB to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_07_12_172743) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "scenes", force: :cascade do |t|
     t.string "blurb"
     t.string "prompt"
